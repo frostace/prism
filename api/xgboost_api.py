59e2ef609b021926bbf4e5ea35dc3e7806fa9609
frostace <frostace0723@gmail.com>
2020-05-03 00:36:03 -0400

    working on visualization with d3.js

--- api/xgboost_api.py
+++ api/xgboost_api.py
@@ -5,17 +5,17 @@ import pandas as pd
 import numpy as np
 
 app = Flask(__name__)
-@app.route('/predict', methods=['POST']) # Your API endpoint URL would consist /predict
+@app.route('/xgboost_api', methods=['POST']) # Your API endpoint URL would consist /predict
 
 def predict():
 	if model:
 		try:
-			print(request)
+			# print(request)
 			json_ = request.json
-			print("json: ", json_)
+			# print("json: ", json_)
 			query = pd.get_dummies(pd.DataFrame(json_))
 			query = query.reindex(columns=model_columns, fill_value=0)
-			print("query: ", query)
+			# print("query: ", query)
 			prediction_scaled = list(model.predict(query))
 
 			return jsonify({'prediction_scaled': str(prediction_scaled)})
@@ -33,12 +33,12 @@ if __name__ == '__main__':
 	except:
 		port = 12345 # If you don't provide any port then the port will be set to 12345
 
-	model_file_name = 'model.pkl'
-	model = joblib.load(model_file_name) # Load "model.pkl"
-	print ('Model loaded')
+	model_file_name = 'xgboost_model.pkl'
+	model = joblib.load(model_file_name) # Load "xgboost_model.pkl"
+	print ('XGBOOST Model loaded')
 
-	model_columns_file_name = 'model_columns.pkl'
-	model_columns = joblib.load(model_columns_file_name) # Load "model_columns.pkl"
-	print ('Model columns loaded')
+	model_columns_file_name = 'xgboost_model_columns.pkl'
+	model_columns = joblib.load(model_columns_file_name) # Load "xgboost_model_columns.pkl"
+	print ('XGBOOST Model columns loaded')
 
 	app.run(port=port, debug=True)
